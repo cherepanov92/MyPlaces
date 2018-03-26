b185b4c110c92dd13880a74803e5169508ed70da
Biowulf513 <cherepanov92@gmail.com>
2018-03-26 18:57:42 +0500

    Создание моделей

--- loco_app/admin.py
+++ loco_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Filial)
+admin.site.register(LocoInFilial)
+admin.site.register(LocoSeries)
+admin.site.register(Mileage)
+
