bae677a8fed1421b03deac1902e9caa7457de138
Biowulf513 <cherepanov92@gmail.com>
2018-03-27 14:13:38 +0500

    приявзка django-pandas к проекту

--- loco_app/views.py
+++ loco_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django_pandas.io import read_frame
 from .models import *
 
 def statistics(request):
     if request.method == 'GET':
-        all_info = Mileage.objects.all()
-        context = {'title': 'index', 'all_info': all_info}
+        qs = Mileage.objects.all()
+        df = read_frame(qs)
+        context = {'title': 'index', 'all_info': df}
+        print(df)
         return render(request, 'index.html', context)
+
+
