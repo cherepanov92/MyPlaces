2d2703db37366772566ba124bd5e2ffd66d9b8bb
Cherepanov Artyom <cherepanov92@gmail.com>
2022-10-25 12:12:00 +0500

    Initial commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by cherepanov92 on 25.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
