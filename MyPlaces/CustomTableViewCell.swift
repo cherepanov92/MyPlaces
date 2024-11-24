a43c7b5e0278ddadf4cf21a459e33d0fbcdaab49
Cherepanov Artyom <cherepanov92@gmail.com>
2024-11-24 22:25:33 +0300

    Добавил кастомный TableViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by cherepanov92 on 25.10.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+}
