95e8310648aa891314deae4bee13678cc36a1502
Cherepanov Artyom <cherepanov92@gmail.com>
2024-11-25 18:05:23 +0300

    Добавил view-создания нового заведения

--- MyPlaces/MainVC.swift
+++ MyPlaces/MainVC.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class MainVC: UITableViewController {
-
-    let restaurantNames = [
-        "Bar Зая", "Bonsai", "Япона Мама",
-        "Паштет", "Вилка-ложка", "Гротт-бар",
-        "dodo pizza", "pizza mia", "Вьетмон",
-        "Сеул", "Золотой лотос"
+    
+    let places = [
+        Place(name: "Bar Зая", location: "Екатеринбург", type: "Бар", image: "Bar Зая"),
+        Place(name: "Bonsai", location: "Екатеринбург", type: "Ресторан", image: "Bonsai"),
+        Place(name: "Япона Мама", location: "Екатеринбург", type: "Семейный ресторан", image: "Япона Мама"),
+        Place(name: "Паштет", location: "Екатеринбург", type: "Ресторан", image: "Паштет"),
+        Place(name: "Вилка-ложка", location: "Екатеринбург", type: "Столовая", image: "Вилка-ложка"),
+        Place(name: "Гротт-бар", location: "Екатеринбург", type: "Бар", image: "Гротт-бар"),
+        Place(name: "dodo pizza", location: "Екатеринбург", type: "Ресторан", image: "dodo pizza"),
+        Place(name: "pizza mia", location: "Екатеринбург", type: "Ресторан", image: "pizza mia"),
+        Place(name: "Вьетмон", location: "Екатеринбург", type: "Ресторан", image: "Вьетмон"),
+        Place(name: "Сеул", location: "Екатеринбург", type: "Ресторан", image: "Сеул"),
+        Place(name: "Золотой лотос", location: "Екатеринбург", type: "Столовая", image: "Золотой лотос")
     ]
     
     override func viewDidLoad() {
@@ -23,27 +30,24 @@ class MainVC: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return restaurantNames.count
+        return places.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
                 
-        cell.imageOfPlace?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageOfPlace?.image = UIImage(named: places[indexPath.row].image)
         cell.imageOfPlace?.layer.cornerRadius = cell.imageOfPlace.frame.size.width / 2
         cell.imageOfPlace?.clipsToBounds = true
         
-        cell.nameLabel?.text = restaurantNames[indexPath.row]
+        cell.nameLabel?.text = places[indexPath.row].name
+        
+        cell.locationLabel.text = places[indexPath.row].location
+        cell.typeLabel.text = places[indexPath.row].type
 
         return cell
     }
     
-    // MARK: - Table view deligate
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 85
-    }
-
     /*
     // MARK: - Navigation
 
@@ -53,5 +57,9 @@ class MainVC: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func cancelAction(_ segue: UIStoryboardSegue) {
+        
+    }
 
 }
---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,16 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by cherepanov92 on 26.10.2022.
+//
+
+import Foundation
+
+struct Place {
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+}
+
