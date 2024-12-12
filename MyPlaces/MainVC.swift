9ed4e724154a1f5611693abc8964f49f19afb754
Cherepanov Artyom <cherepanov92@gmail.com>
2024-12-12 21:27:11 +0500

    Переписал UITableViewController на UIViewController

--- MyPlaces/MainVC.swift
+++ MyPlaces/MainVC.swift
@@ -8,7 +8,8 @@
 import UIKit
 import RealmSwift
 
-class MainVC: UITableViewController {
+class MainVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
     
     var places: Results<Place>!
         
@@ -20,11 +21,11 @@ class MainVC: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return places.isEmpty ? 0 : places.count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         let place = places[indexPath.row]
         
@@ -42,7 +43,7 @@ class MainVC: UITableViewController {
     }
     
     // MARK: - Table view deligate
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             let place = places[indexPath.row]
             StorageManager.deleteObject(place)
